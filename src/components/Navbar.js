b671e7e23314a503f89bea8e5b17f04330be9a78
samju200 <samju7778@gmail.com>
2023-11-29 15:32:29 +0100

    route back to login without user

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Logo from "../img/logo1.jpeg";
 import { IoIosNotifications } from "react-icons/io";
 import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
 function Navbar() {
   const user = useSelector((state) => state.auth.user);
   const dispatch = useDispatch();
+  let navigate = useNavigate();
   const logoutUser = () => {
     dispatch(logout());
   };
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
   return (
     <div className="header">
       <div className="header-logo">
